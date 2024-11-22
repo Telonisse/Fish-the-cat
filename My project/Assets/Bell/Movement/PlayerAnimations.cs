2970346b49a83cdacb6e12cb1181f4b6f6d2d88e
Minisasy <Isabell.ljungberg2004@gmail.com>
2024-11-22 12:19:42 +0100

    start anim

---
+++ My project/Assets/Bell/Movement/PlayerAnimations.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+    [SerializeField] float maxSpeed= 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
